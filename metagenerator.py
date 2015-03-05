6f750728c37263b71f701680f1b12d38e41440d6
Asher <asherkhb@gmail.com>
2015-03-05 15:17:50 -0700

    Updates 3/5/15 #2

--- metagenerator.py
+++ metagenerator.py
@@ -1,16 +1,22 @@
 __author__ = 'asherkhb'
+# Metadata Generator Script, v.1.0.0
+#
+# by Asher Baltzell
 
-#Takes Dataset as a dictionary with {huID:[link, health]} layout
-
+#Function to generate metadata file.
+#Future Updates: Unique Metadata Name
+#Input: Dictionary dataset, format {huID:[link, health]}
+# Current Metadata Inclusions: Filename*, Name*, Description, Source, Source Link, Sequencer, Health Info
+# "*" Denotes CoGe Required Fields
 def generateMeta(dataset):
     with open('meta.txt', 'w') as metadata:
         metadata.write("Filename\tName\tDescription\tSource\tSource_link\tSequencer\tHealth Records\n")
         for key in dataset.keys():
             meta = generateItemMeta(key, dataset[key][0], dataset[key][1])
             metadata.write(meta)
-    print "Complete. See meta.txt"
 
-
+#Function to generate individual metadata entries for each experiment
+#Requires huID, download link, and health info inputs
 def generateItemMeta(huID, link, health):
     filename = "%s_snps.vcf" % huID
     name = "%s_snps" % huID
--- snpscraper/snpscraper/spiders/snp_spider.py
+++ snpscraper/snpscraper/spiders/snp_spider.py
@@ -3,15 +3,15 @@ import scrapy
 from snpscraper.items import SnpSet
 
 class SnpSpider(scrapy.Spider):
-    name = "snp"
-    allowed_domains = ["pgp-hms.org"]
-    start_urls = [
-        "https://my.pgp-hms.org/public_genetic_data?utf8=%E2%9C%93&data_type=23andMe&commit=Search"
-    ]
+	name = "snp"
+	allowed_domains = ["pgp-hms.org"]
+	start_urls = [
+		"https://my.pgp-hms.org/public_genetic_data?utf8=%E2%9C%93&data_type=23andMe&commit=Search"
+	]
 
-    def parse(self, response):
-        for sel in response.xpath('//div[@class="profile-data"]//tr'):
-	    item = SnpSet()
-            item['huid'] = sel.xpath('td[@data-summarize-as="participant"]/a/text()').extract()
-	    item['link'] = sel.xpath('td[@data-summarize-as="size"]/a/@href').extract()
-            yield item
+	def parse(self, response):
+		for sel in response.xpath('//div[@class="profile-data"]//tr'):
+		item = SnpSet()
+			item['huid'] = sel.xpath('td[@data-summarize-as="participant"]/a/text()').extract()
+		item['link'] = sel.xpath('td[@data-summarize-as="size"]/a/@href').extract()
+			yield item
