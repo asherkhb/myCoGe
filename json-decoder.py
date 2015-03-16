d1490dfc3c2e7f615e8aaf49a1759ef27eefa0e9
Asher <asherkhb@gmail.com>
2015-03-16 16:09:06 -0700

    json_decoder.py v.1.2.0 - Functional/stylistic changes to work with other updated scripts. Updated to reflect PEP8 style conventions.

--- json-decoder.py
+++ json-decoder.py
@@ -1,28 +1,70 @@
+__author__ = 'asherkhb'
 # SNPScraper JSON Object Decoding Script
-# Converts JSON objects produced by snpscraper web-crawler into a dictionary of huIDs (keys) download links(values)
+# Converts JSON objects produced by SNPscraper into a dictionary {huID, [download link, health info] ... }
 #
-#by Asher Baltzell
+# by Asher Baltzell
+# Last Update: 3/16/15
+# Version 1.2.0
 
 import json
-import metagenerator
 
-data_dict = {}
+practice = './snpscraper/snps_3-1-15.json'
 
-with open('./snpscraper/snps_3-1-15.json', 'r') as data:
-    jdata = json.load(data)
 
-    for i in range(0,len(jdata)):
-        entry = jdata[i]
-        huid = str(entry[u'huid']).strip('[').strip(']').strip('u').strip("'")
-        raw_link = str(entry[u'link']).strip('[').strip(']').strip('u').strip("'")
-        link = "https://my.pgp-hms.org%s" % raw_link
-        if huid != '':
-            if raw_link != '':
+def json_decode(snp_json):
+    """Decode SNPScraper JSON Output
+
+    A function to convert JSON objects outputted by SNPScraper into a python dictionary.
+    Dictionary can then be used for obtaining data and generating metadata.
+
+    Arguments:
+      snp_json: JSON output from SNPScraper
+
+    Returns:
+      Dictionary of huIDs, download links, and health info
+        Dictionary Structure: {key:values} of {'huID': ['download-link', 'health-info'], ... }
+    """
+    #Define empty output dictionary.
+    data_dict = {}
+
+    #Open the JSON object for processing.
+    with open(snp_json, 'r') as data:
+        #Load JSON data into variable jdata.
+        jdata = json.load(data)
+        #Iterate through jdata.
+        for i in range(0, len(jdata)):
+            #Set entry to current object.
+            entry = jdata[i]
+            #Assign huID, strip unwanted characters.
+            huid = str(entry[u'huid']).strip('[').strip(']').strip('u').strip("'")
+            #Assign raw link, strip unwanted characters.
+            raw_link = str(entry[u'link']).strip('[').strip(']').strip('u').strip("'")
+            #Define link based on raw-link.
+            link = "https://my.pgp-hms.org%s" % raw_link
+            #If huID and raw-link are present, add entry to dictionary.
+            if huid != '' and raw_link != '':
                 data_dict[huid] = [link, "Health Filler"]
+    #Return dictionary.
+    return data_dict
 
-    #ACCESS ALL HUIDS & LINKS
-    #for key in data_dict.keys():
-    #    print key, data_dict[key]
 
-#Use Metagenerator to Generate Metadata File for datapoints in data_dict
-metagenerator.generateMeta(data_dict)
+def write_acquired_data(dataset):
+    """Write Out Contents from Decoded JSON Dictionary
+
+    A function to write a summary text file of data sets (huIDs and associated links) from a converted JSON
+    Prints summary file "acquired_data.txt" to working directory
+
+    Arguments:
+      dataset: Dictionary containing data-sets, typically obtained from json_decode.
+
+    Returns:
+      None - Generates and writes "acquired_data.txt" summary file.
+    """
+    with open('acquired_data.txt', 'w') as otpt:
+        #Iterate through dataset
+        for key in dataset.keys():
+            #Write out lines with tab-separated huID and download link.
+            writeout = "%s\t%s\n" % (key, dataset[key][0])
+            otpt.write(writeout)
+
+write_acquired_data(json_decode(practice))
