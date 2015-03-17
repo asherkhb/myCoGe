7132aca00bb6bd8d78b4866e90ca02b49c351c2c
Asher <asherkhb@gmail.com>
2015-03-16 17:33:04 -0700

    metagenerator.py v.1.0.1 - Added docstrings, styles updated to conform with PEP 8.

--- metagenerator.py
+++ metagenerator.py
@@ -1,29 +1,63 @@
 __author__ = 'asherkhb'
 # Metadata Generator Script, v.1.0.0
+# Generates metadata for SNP variant files for import into CoGe
 #
 # by Asher Baltzell
+# Last Update: 3/16/15
+# Version: 1.0.1
 
-#Function to generate metadata file.
-#Future Updates: Unique Metadata Name
-#Input: Dictionary dataset, format {huID:[link, health]}
-# Current Metadata Inclusions: Filename*, Name*, Description, Source, Source Link, Sequencer, Health Info
-# "*" Denotes CoGe Required Fields
-def generateMeta(dataset):
+
+def generate_meta(dataset):
+    """Generate Metadata TSV File
+
+    A function to generate a TSV metadata file describing experiments contained in a dictionary.
+    Optimized for dictionaries generated with json-decoder.py
+    Current Metadata: Filename*, Name*, Description, Source, Source Link, Sequencer, Health Info
+    "*" Denotes CoGe Required Fields
+
+    Arguments:
+      dataset: Python dictionary with datasets.
+        Dictionary Structure: {huID:[link, health], ...}
+
+    Returns:
+      None - Generates and writes metadata file "meta.txt".
+    """
     with open('meta.txt', 'w') as metadata:
+        #Write head row
         metadata.write("Filename\tName\tDescription\tSource\tSource_link\tSequencer\tHealth Records\n")
+        #Iterate through data-set, write each data point meta entry.
         for key in dataset.keys():
-            meta = generateItemMeta(key, dataset[key][0], dataset[key][1])
+            meta = generate_item_meta(key, dataset[key][0], dataset[key][1])
             metadata.write(meta)
 
-#Function to generate individual metadata entries for each experiment
-#Requires huID, download link, and health info inputs
-def generateItemMeta(huID, link, health):
-    filename = "%s_snps.vcf" % huID
-    name = "%s_snps" % huID
-    description = "SNP Variant Data for %s, obtained from Personal Genome Project (www.personalgenomes.org)" % huID
+
+def generate_item_meta(huid, link, health):
+    """Generate Metadata for Individual Experiment
+
+    A function to generate a TSV metadata entry for an individual experiment.
+    Optimized for generating entries for generate_meta function
+    Current Metadata: Filename*, Name*, Description, Source, Source Link, Sequencer, Health Info
+    "*" Denotes CoGe Required Fields
+
+    Arguments:
+      huid: huID identifying experiment.
+      link: Download link for experiment (orignal source).
+      health: Public health records.
+
+    Returns:
+      meta - TSV metadata entry for experiment.
+    """
+    #Assign Variables
+    filename = "%s_snps.vcf" % huid
+    name = "%s_snps" % huid
+    description = "SNP Variant Data for %s, obtained from Personal Genome Project (www.personalgenomes.org)" % huid
     source = "PGP"
     source_link = link
     seq = "23andMe"
     health_info = health
+    #Generate metadata entry
     meta = "%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (filename, name, description, source, source_link, seq, health_info)
     return meta
+
+# Use Metagenerator to Generate Metadata File for datapoints in data_dict
+# generate_meta(dataset_from_json_decoder)
