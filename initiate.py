fa04ce0b4f5182dbdd836598588a3557249f6627
Asher <asherkhb@gmail.com>
2015-04-24 12:01:14 -0700

    Initiate practice version 1

--- initiate.py
+++ initiate.py
@@ -16,7 +16,7 @@ run_date = myCoGe.datetime.now().strftime("%Y%m%d")
 #       - Return Dictionary of huIDs, download links, health info
 
 ##json_file = './temp/snps_%s.json' % run_date
-json_file = './temp/practice_1.json'
+json_file = './temp/practice_1.json' # For Practice
 simple_data, all_data = myCoGe.json_decode(json_file)
 
 # 3. Compare dictionary with File Directory (_directory.txt)
@@ -28,7 +28,7 @@ missing_data = myCoGe.compare_to_directory(all_data)
 # 4. Download Data
 #       - Download missing data fileset.
 
-repo = './temp/vcfs'
+repo = './temp/tsvs'
 downloaded = myCoGe.get_data(missing_data, repo)
 
 # 5. RESOLVE DOWNLOADED/MISSING, GENERATE NEW DICT UPDATE DICT, and PRINT ERROR REPORTS
@@ -49,14 +49,16 @@ myCoGe.update_directory(downloads)
 
 for item in downloads:
     file_huid = item
-    filename = './temp/vcfs/%s.txt' % item
-    vcfname = './data/data/vcfs/%s.vcf' % item
+    filename = './temp/tsvs/%s.txt' % item
+    vcfname = './data/vcfs/%s.vcf' % item
 
     experiment = myCoGe.SnpExperiment(file_huid, filename, vcfname)
     experiment.convert_to_vcf()
 
 # 9. Transfer files to iRODS (Partial)
 #
+
+
 
 # 10. Batch load into CoGe (Incomplete)
 #
