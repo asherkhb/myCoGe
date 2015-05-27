98f4d446f0722ad29ad249d2628922f0afe13d8f
Asher <asherkhb@gmail.com>
2015-05-27 12:15:33 -0700

    modifications

--- unzipper.py
+++ unzipper.py
@@ -38,30 +38,25 @@ if start:
         file_path = '%s.zip' % zippedfile
         print "Processing %s" % file_path
         #Create a ZipFile object.
-        try:
-            zip_file = ZipFile(file_path)
-            #Create a list of ZipFile contents.
-            zip_list = ZipFile.namelist(zip_file)
-            print "The file contains: " + zip_list
-            cont = get_confirmation("Continue with unzipping and renaming %s?" % zip_list[0])
-            if cont:
-                #Create variables with old content name and new (huID) content name.
-                old_name = zip_list[0]
-                new_name = file_path.replace('.zip', '.txt')
-                #Unzip ZipFile.
-                #unzip = "unzip %s -d %s " % (file_path, repository)  # Saved in-case
-                unzip = "unzip %s" % file_path
-                call(unzip, shell=True)
-                #Rename file contents with huID and then remove zip file.
-                rename(old_name, new_name)
-                new_path = '../tsvs/%s' % new_name
-                move(new_name, new_path)
-                remove(file_path)
-            else:
-                continue
-        except:
-            cont = get_confirmation("Error Unzipping %s. Continue?" % zippedfile)
-            if not cont:
-                exit()
+        zip_file = ZipFile(file_path)
+        #Create a list of ZipFile contents.
+        zip_list = ZipFile.namelist(zip_file)
+        print "The file contains: " + zip_list
+        cont = get_confirmation("Continue with unzipping and renaming %s?" % zip_list[0])
+        if cont:
+            #Create variables with old content name and new (huID) content name.
+            old_name = zip_list[0]
+            new_name = file_path.replace('.zip', '.txt')
+            #Unzip ZipFile.
+            #unzip = "unzip %s -d %s " % (file_path, repository)  # Saved in-case
+            unzip = "unzip %s" % file_path
+            call(unzip, shell=True)
+            #Rename file contents with huID and then remove zip file.
+            rename(old_name, new_name)
+            new_path = '../tsvs/%s' % new_name
+            move(new_name, new_path)
+            remove(file_path)
+        else:
+            continue
 else:
     exit()
